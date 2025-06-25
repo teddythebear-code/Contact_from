30985553e4f6a73176e2aef1729aba9b38241506
nosaozemoyaefeanthony <nosaozemoyaefeanthony@gmail.com>
2025-06-25 12:05:03 +0100

    Update script.js

---
+++ APP/JS/script.js
@@ -0,0 +1,5 @@
+//the HTML id element 
+
+const firstname = document.getElementById("firstname");
+const secondname = document.getElementById("secondname");
+const message = document.getElementById("message");
