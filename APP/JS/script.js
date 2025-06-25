5f2093832c95757a4c9ecf62bf799a1c034e717b
nosaozemoyaefeanthony <nosaozemoyaefeanthony@gmail.com>
2025-06-25 12:58:06 +0100

    Update script.js

--- APP/JS/script.js
+++ APP/JS/script.js
@@ -3,3 +3,16 @@
 const firstname = document.getElementById("firstname");
 const secondname = document.getElementById("secondname");
 const message = document.getElementById("message");
+
+//
+function sendemailjs();
+const templateParams = {
+firstname.value,
+secondname.value,
+message.valus,
+};
+
+//
+emailjs.send("service_12phka2", "template_mmeoe47", templateParams).then(
+() => alert("send mail").catch(() => alert("send not mail")));
+}
