73cfd971c60f1b03edaeb5f5e06fd8cedb52cd27
teddythebear <nosaozemoyaefeanthony@gmail.com>
2025-06-25 16:32:38 +0100

    fix all code error

--- APP/JS/script.js
+++ APP/JS/script.js
@@ -4,15 +4,17 @@ const firstname = document.getElementById("firstname");
 const secondname = document.getElementById("secondname");
 const message = document.getElementById("message");
 
-//
-function sendemailjs();
+//the form sendemailjs function
+function sendemailjs(){
+
+//the objcet form the element in a
 const templateParams = {
-firstname.value,
-secondname.value,
-message.valus,
+firstname :firstname.value,
+secondname: secondname.value,
+message: message.valus,
 };
 
-//
+//the sendemailjs program
 emailjs.send("service_12phka2", "template_mmeoe47", templateParams).then(
 () => alert("send mail").catch(() => alert("send not mail")));
 }
